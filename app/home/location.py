aec4e2a47e9434eb8b1569c0577582f8d7743c25
luodongseu <1576848284@qq.com>
2016-05-03 15:25:10 +0800

    add location real data

--- app/home/location.py
+++ app/home/location.py
@@ -4,11 +4,29 @@
 
 import web
 import config
+import sys
+
+sys.path.append("..")
+from api.common import Common
+from api.apiManager import ApiManager
+from const import errors
 
 
 class Location:
     def GET(self):
+        '''
+        静态查看定位信息
+        :return:
+        '''
         data = web.input()
         if 'yesterday' in data.keys():
             return config.render.location_history(data)
+
+        username = Common.getLoginUsername()  # 获得登录用户名
+        r, data = ApiManager.getDeviceLocationInfo(username)  # 获取设备定位信息
+        if r == 'fail':  # 获取设备信息失败
+            if data == errors.NOT_BIND:  # 如果未绑定 则进入绑定页
+                return web.redirect('/bind?username=' + username)
+            else:  # 否则进入404
+                return web.redirect('/404')
         return config.render.location(data)
