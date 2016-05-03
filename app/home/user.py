296f00e84ffb4ab6adc8deb717304429480925b9
luodongseu <1576848284@qq.com>
2016-05-03 15:21:17 +0800

    add user real data

--- app/home/user.py
+++ app/home/user.py
@@ -4,11 +4,26 @@
 
 import web
 import config
-# Templates
+import sys
+
+sys.path.append("..")
+from api.common import Common
+from api.apiManager import ApiManager
+from const import errors
 
 
 class User:
-
     def GET(self):
-        data = web.input()
+        '''
+        静态查看用户基本信息
+        :return:
+        '''
+
+        username = Common.getLoginUsername()  # 获得登录用户名
+        r, data = ApiManager.getUserInfo(username)  # 获取设备信息
+        if r == 'fail':  # 获取设备信息失败
+            if data == errors.NOT_BIND:  # 如果未绑定 则进入绑定页
+                return web.redirect('/bind?username=' + username)
+            else:  # 否则进入404
+                return web.redirect('/404')
         return config.render.user(data)
