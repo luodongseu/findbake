b7f1bb8cf4052aa6daaf27e3167785bcf6db9be9
luodongseu <1576848284@qq.com>
2016-05-03 13:51:49 +0800

    add login check code

--- app/api/common.py
+++ app/api/common.py
@@ -4,6 +4,29 @@
 公共函数库
 '''
 
-# import web
-#
-# class Common:
+import web
+
+
+class Common:
+    def islogin(self):
+        '''
+        判断用户是否登录
+        :return: 登录后返回用户名 未登录直接跳转到绑定界面
+        '''
+
+        try:
+            session = web.ctx.session
+            username = session.username
+            if not username:
+                return web.redirect('/bind')
+            else:
+                return username
+        except Exception as e:
+            return web.redirect('/bind')
+
+    def getLoginUsername(self):
+        '''
+        获取登录的用户名
+        :return:
+        '''
+        return self.islogin()
