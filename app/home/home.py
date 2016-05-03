73430bba9c17223cfc08d2d8fc2ef640e10f3fd4
luodongseu <1576848284@qq.com>
2016-05-03 14:46:45 +0800

    add 404 page

--- app/home/home.py
+++ app/home/home.py
@@ -10,11 +10,36 @@ import sys
 sys.path.append("..")
 
 from weixin.jshelper import JSHelper
+from api.common import Common
+from api.apiManager import ApiManager
+from const import errors
 
 
 class Home:
     def GET(self):
-        """ Show home page """
-        # input = web.input()
+        '''
+        检测是否登录
+        :return:
+        '''
+        input = web.input(username=None)
+        username = input.username
+        if not username:  # 如果没有用户名参数,则进入404
+            return web.redirect('/404')  # 进入404页面
+        self.login(username)
         data = JSHelper.js_data(web.ctx.homedomain + web.ctx.homepath + web.ctx.fullpath)
         return config.render.home(data)
+
+    def login(self, username):
+        '''
+        用户登录
+        :param username:用户名
+        :return:
+        '''
+        r, msg = ApiManager.login(username)
+        if r == 'success':  # 登录成功,则将返回来的用户ID加入session
+            Common.addsession(msg)
+        else:
+            if msg == errors.NOT_BIND:  # 未绑定
+                return web.redirect('/bind?username=' + username)
+            else:
+                return web.redirect('/404')  # 进入404页面
