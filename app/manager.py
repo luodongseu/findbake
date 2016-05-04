90a3fdeaad280254130d25f3a096818daa30495a
luodongseu <1576848284@qq.com>
2016-05-04 21:56:24 +0800

    fix bug

--- app/manager.py
+++ app/manager.py
@@ -1,19 +1,8 @@
 # coding: UTF-8
+
 '''
 硬件设备访问 入口文件
 '''
-
-import web
-import config
-
-import sys
-
-sys.path.append("..")
-
-from weixin.jshelper import JSHelper
-from api.common import Common
-from api.apiManager import ApiManager
-from const import errors
 
 
 class Manager:
