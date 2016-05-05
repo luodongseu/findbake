47cc05bf63c606c47cfa7e21c3ef817ef6c098ca
luodongseu <1576848284@qq.com>
2016-05-05 10:55:44 +0800

    add device refresh api

--- app/manager.py
+++ app/manager.py
@@ -5,6 +5,7 @@
 '''
 
 import web
+from api.apiManager import ApiManager
 
 
 class Manager:
@@ -21,4 +22,20 @@ class Manager:
         设备访问POST请求
         :return:
         '''
-        return 'hello findbake'
+        input = web.input(ccid=None, gps=None, power=None)
+        ccid = input.ccid
+        gps = input.gps
+        power = input.power
+        if not ccid or not gps:
+            return 'fail'
+        if not power or power not in [0, 100]:
+            power = 100
+        d = {
+            'gps': gps,
+            'power': power
+        }
+        r, m = ApiManager.refreshDeviceInfo(ccid, d)
+        if r == 'success':
+            return m
+        else:
+            return 'success'
