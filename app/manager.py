eef455895f81145555c3eaac2210a983406cefe4
luodongseu <1576848284@qq.com>
2016-05-14 21:27:39 +0800

    test sound on order

--- app/manager.py
+++ app/manager.py
@@ -19,7 +19,7 @@ class Manager:
         设备访问GET请求
         :return:
         '''
-
+        return '{201:S_O}'
         input = web.input(data=None, lat=None, lon=None, ccid=None)
         data = input.data  # 数据类型 -1/0/1...
         # return '{'+data+'}'
