016f01afefdfeb8a1d0751183dcab69f1d87b69f
luodongseu <1576848284@qq.com>
2016-05-14 18:31:11 +0800

    update order code

--- app/const/orders.py
+++ app/const/orders.py
@@ -11,22 +11,22 @@ orders.START_SYSTEM
 '''
 
 '''重置系统'''
-REST_SYSTEM = 'RS'
+REST_SYSTEM = 'R'
 
 '''重置设备刷新频率'''
-REFRESH_RATE_REST = 'RRR'
+REFRESH_RATE_REST = 'F_R'
 
 '''设备刷新频率高'''
-REFRESH_RATE_HIGH = 'RRH'
+REFRESH_RATE_HIGH = 'F_H'
 
 '''打开声音设备'''
-OPEN_SOUND = 'OS'
+OPEN_SOUND = 'S_O'
 
 '''关闭声音设备'''
-CLOSE_SOUND = 'CS'
+CLOSE_SOUND = 'S_C'
 
 '''开机'''
-START_SYSTEM = 'SSYS'
+START_SYSTEM = 'D_O'
 
 '''关机'''
-CLOSE_SYSTEM = 'CSYS'
+CLOSE_SYSTEM = 'D_C'
