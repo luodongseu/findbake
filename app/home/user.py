9818c42f7ab6ccd95bb4735c91e015841fe635d0
luodongseu <1576848284@qq.com>
2016-05-15 13:23:35 +0800

    fix bug

--- app/home/user.py
+++ app/home/user.py
@@ -22,11 +22,11 @@ class User:
         if not username:
             return 'No User'
         return username
-        r, data = ApiManager.getUserInfo(username)  # 获取设备信息
-        if r == 'fail':  # 获取设备信息失败
-            if data == errors.NOT_BIND:  # 如果未绑定 则进入绑定页
-                url = '/bind?username=' + username
-                return web.redirect(url)
-            else:  # 否则进入404
-                return web.redirect('/404')
-        return config.render.user(data)
+        # r, data = ApiManager.getUserInfo(username)  # 获取设备信息
+        # if r == 'fail':  # 获取设备信息失败
+        #     if data == errors.NOT_BIND:  # 如果未绑定 则进入绑定页
+        #         url = '/bind?username=' + username
+        #         return web.redirect(url)
+        #     else:  # 否则进入404
+        #         return web.redirect('/404')
+        # return config.render.user(data)
