5f6047efcf1a0b74495f6bd42e72835ef8f73ad4
luodongseu <1576848284@qq.com>
2016-05-16 08:24:00 +0800

    fix sound bugs

--- app/home/device.py
+++ app/home/device.py
@@ -30,3 +30,29 @@ class Device:
             else:  # 否则进入404
                 return web.redirect('/404')
         return config.render.device(data)
+
+    def POST(self):
+        '''
+        接收指令
+        :return:
+        '''
+        input = web.input(delay=None)
+        delay = input.delay
+        if not delay:
+            return 'failed'
+        if isinstance(delay, (int)):
+            username = Common.getLoginUsername()  # 获得登录用户名
+            if not username:  # 不存在则返回重定向
+                return web.redirect('/404')
+
+            r, data = ApiManager.getDeviceInfo(username)  # 获取设备信息
+            if r == 'fail':  # 获取设备信息失败
+                if data == errors.NOT_BIND:  # 如果未绑定 则进入绑定页
+                    url = '/bind?username=' + username
+                    return web.redirect(url)
+                else:  # 否则进入404
+                    return web.redirect('/404')
+            else:  # 成功获取信息
+                ApiManager.sendOrder(data['id'], 'S_' + str(delay))  # 发送S_X指令
+                return 'success'
+        return 'fail'
