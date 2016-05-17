174436d490526988f011a2586e1bff87b37aeb48
luodongseu <1576848284@qq.com>
2016-05-17 23:06:41 +0800

    fix bugs

--- app/const/errors.py
+++ app/const/errors.py
@@ -11,9 +11,9 @@ errors.NOT_LOGIN
 '''非微信用户使用'''
 NOT_WXUSER = '用户异常'
 '''未绑定设备'''
-NOT_BIND = '未绑定设备'
+NOT_BIND = '未绑定设备'.encode("UTF-8")
 '''设备不存在'''
-NO_DEVICE = '设备不存在'
+NO_DEVICE = '设备不存在'.encode("UTF-8")
 '''系统故障'''
 ERROR_SYSTEM = '系统故障'
 '''参数错误'''
